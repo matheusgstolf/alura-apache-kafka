2b135b53d84bf05315887246eb30bfb265a18fac
Matheus Gutjahr Stolf <matheusgstolf@gmail.com>
2023-03-16 06:14:55 -0300

    Ajustado configurações do maven e importações das classes

--- src/main/java/br/com/alura/NewOrderMain.java
+++ src/main/java/br/com/alura/NewOrderMain.java
@@ -2,11 +2,19 @@ package br.com.alura;
 
 import java.util.Properties;
 import java.util.Map;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 public class NewOrderMain {
     public static void main(String[] args) {
         var producer = new KafkaProducer<String, String>(properties());
+        var value = "1,1,500.00";
+        var record = new ProducerRecord<String,String>("ECOMMERCE_NEW_ORDER",value);
+        producer.send(record);
     }
 
     private static Properties properties() {
