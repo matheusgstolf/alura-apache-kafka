85f45912e9a7cac538e819bcb19f10fdbea13205
Matheus Gutjahr Stolf <matheusgstolf@gmail.com>
2023-03-17 21:06:15 -0300

    Implementando mais custumers, criando log para ler mais de um tópico via REGEX

--- src/main/java/br/com/alura/NewOrderMain.java
+++ src/main/java/br/com/alura/NewOrderMain.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -15,14 +16,18 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "132123, 67523, 7894589745";
+        var email = "matheusgstolf@gmail.com";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",value, value);
-        producer.send(record, (data, ex) -> {
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+        };
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
